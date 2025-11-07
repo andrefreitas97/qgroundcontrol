f8bb4898c0f17311ae4cc1d30095175519f8af2d
andrefreitas97 <antonioandre1997@gmail.com>
2025-11-07 11:38:27 +0000

    ESC telem extend for 9 to 12

--- src/Vehicle/VehicleEscStatusFactGroup.h
+++ src/Vehicle/VehicleEscStatusFactGroup.h
@@ -31,6 +31,10 @@ public:
     Q_PROPERTY(Fact* rpmSixth           READ rpmSixth           CONSTANT)
     Q_PROPERTY(Fact* rpmSeventh         READ rpmSeventh         CONSTANT)
     Q_PROPERTY(Fact* rpmEight           READ rpmEight           CONSTANT)
+    Q_PROPERTY(Fact* rpmNinth           READ rpmNinth               CONSTANT)
+    Q_PROPERTY(Fact* rpmTenth           READ rpmTenth               CONSTANT)
+    Q_PROPERTY(Fact* rpmEleventh        READ rpmEleventh            CONSTANT)
+    Q_PROPERTY(Fact* rpmTwelfth         READ rpmTwelfth             CONSTANT)
 
     Q_PROPERTY(Fact* currentFirst       READ currentFirst       CONSTANT)
     Q_PROPERTY(Fact* currentSecond      READ currentSecond      CONSTANT)
@@ -40,6 +44,10 @@ public:
     Q_PROPERTY(Fact* currentSixth       READ currentSixth       CONSTANT)
     Q_PROPERTY(Fact* currentSeventh     READ currentSeventh     CONSTANT)
     Q_PROPERTY(Fact* currentEight       READ currentEight       CONSTANT)
+    Q_PROPERTY(Fact* currentNinth       READ currentNinth           CONSTANT)
+    Q_PROPERTY(Fact* currentTenth       READ currentTenth           CONSTANT)
+    Q_PROPERTY(Fact* currentEleventh    READ currentEleventh        CONSTANT)
+    Q_PROPERTY(Fact* currentTwelfth     READ currentTwelfth         CONSTANT)
 
     Q_PROPERTY(Fact* voltageFirst       READ voltageFirst       CONSTANT)
     Q_PROPERTY(Fact* voltageSecond      READ voltageSecond      CONSTANT)
@@ -49,6 +57,10 @@ public:
     Q_PROPERTY(Fact* voltageSixth       READ voltageSixth       CONSTANT)
     Q_PROPERTY(Fact* voltageSeventh     READ voltageSeventh     CONSTANT)
     Q_PROPERTY(Fact* voltageEight       READ voltageEight       CONSTANT)
+    Q_PROPERTY(Fact* voltageNinth       READ voltageNinth           CONSTANT)
+    Q_PROPERTY(Fact* voltageTenth       READ voltageTenth           CONSTANT)
+    Q_PROPERTY(Fact* voltageEleventh    READ voltageEleventh        CONSTANT)
+    Q_PROPERTY(Fact* voltageTwelfth     READ voltageTwelfth         CONSTANT)
 
     Q_PROPERTY(Fact* temperatureFirst       READ temperatureFirst       CONSTANT)
     Q_PROPERTY(Fact* temperatureSecond      READ temperatureSecond      CONSTANT)
@@ -58,6 +70,10 @@ public:
     Q_PROPERTY(Fact* temperatureSixth       READ temperatureSixth       CONSTANT)
     Q_PROPERTY(Fact* temperatureSeventh     READ temperatureSeventh     CONSTANT)
     Q_PROPERTY(Fact* temperatureEight       READ temperatureEight       CONSTANT)
+    Q_PROPERTY(Fact* temperatureNinth       READ temperatureNinth       CONSTANT)
+    Q_PROPERTY(Fact* temperatureTenth       READ temperatureTenth       CONSTANT)
+    Q_PROPERTY(Fact* temperatureEleventh    READ temperatureEleventh    CONSTANT)
+    Q_PROPERTY(Fact* temperatureTwelfth     READ temperatureTwelfth     CONSTANT)
 
     //Fact* index                         () { return &_indexFact; }
 
@@ -69,6 +85,10 @@ public:
     Fact* rpmSixth                      () { return &_rpmSixthFact; }
     Fact* rpmSeventh                    () { return &_rpmSeventhFact; }
     Fact* rpmEight                      () { return &_rpmEightFact; }
+    Fact* rpmNinth                      () { return &_rpmNinthFact; }
+    Fact* rpmTenth                      () { return &_rpmTenthFact; }
+    Fact* rpmEleventh                   () { return &_rpmEleventhFact; }
+    Fact* rpmTwelfth                    () { return &_rpmTwelfthFact; }
 
     Fact* currentFirst                  () { return &_currentFirstFact; }
     Fact* currentSecond                 () { return &_currentSecondFact; }
@@ -78,6 +98,10 @@ public:
     Fact* currentSixth                  () { return &_currentSixthFact; }
     Fact* currentSeventh                () { return &_currentSeventhFact; }
     Fact* currentEight                  () { return &_currentEightFact; }
+    Fact* currentNinth                  () { return &_currentNinthFact; }
+    Fact* currentTenth                  () { return &_currentTenthFact; }
+    Fact* currentEleventh               () { return &_currentEleventhFact; }
+    Fact* currentTwelfth                () { return &_currentTwelfthFact; }
 
     Fact* voltageFirst                  () { return &_voltageFirstFact; }
     Fact* voltageSecond                 () { return &_voltageSecondFact; }
@@ -87,6 +111,10 @@ public:
     Fact* voltageSixth                  () { return &_voltageSixthFact; }
     Fact* voltageSeventh                () { return &_voltageSeventhFact; }
     Fact* voltageEight                  () { return &_voltageEightFact; }
+    Fact* voltageNinth                  () { return &_voltageNinthFact; }
+    Fact* voltageTenth                  () { return &_voltageTenthFact; }
+    Fact* voltageEleventh               () { return &_voltageEleventhFact; }
+    Fact* voltageTwelfth                () { return &_voltageTwelfthFact; }
 
     Fact* temperatureFirst                  () { return &_temperatureFirstFact; }
     Fact* temperatureSecond                 () { return &_temperatureSecondFact; }
@@ -96,6 +124,10 @@ public:
     Fact* temperatureSixth                  () { return &_temperatureSixthFact; }
     Fact* temperatureSeventh                () { return &_temperatureSeventhFact; }
     Fact* temperatureEight                  () { return &_temperatureEightFact; }
+    Fact* temperatureNinth              () { return &_temperatureNinthFact; }
+    Fact* temperatureTenth              () { return &_temperatureTenthFact; }
+    Fact* temperatureEleventh           () { return &_temperatureEleventhFact; }
+    Fact* temperatureTwelfth            () { return &_temperatureTwelfthFact; }
 
     // Overrides from FactGroup
     void handleMessage(Vehicle* vehicle, mavlink_message_t& message) override;
@@ -110,6 +142,10 @@ public:
     static const char* _rpmSixthFactName;
     static const char* _rpmSeventhFactName;
     static const char* _rpmEightFactName;
+    static const char* _rpmNinthFactName;
+    static const char* _rpmTenthFactName;
+    static const char* _rpmEleventhFactName;
+    static const char* _rpmTwelfthFactName;
 
     static const char* _currentFirstFactName;
     static const char* _currentSecondFactName;
@@ -119,6 +155,10 @@ public:
     static const char* _currentSixthFactName;
     static const char* _currentSeventhFactName;
     static const char* _currentEightFactName;
+    static const char* _currentNinthFactName;
+    static const char* _currentTenthFactName;
+    static const char* _currentEleventhFactName;
+    static const char* _currentTwelfthFactName;
 
     static const char* _voltageFirstFactName;
     static const char* _voltageSecondFactName;
@@ -128,6 +168,10 @@ public:
     static const char* _voltageSixthFactName;
     static const char* _voltageSeventhFactName;
     static const char* _voltageEightFactName;
+    static const char* _voltageNinthFactName;
+    static const char* _voltageTenthFactName;
+    static const char* _voltageEleventhFactName;
+    static const char* _voltageTwelfthFactName;
 
     static const char* _temperatureFirstFactName;
     static const char* _temperatureSecondFactName;
@@ -137,6 +181,11 @@ public:
     static const char* _temperatureSixthFactName;
     static const char* _temperatureSeventhFactName;
     static const char* _temperatureEightFactName;
+    static const char* _temperatureNinthFactName;
+    static const char* _temperatureTenthFactName;
+    static const char* _temperatureEleventhFactName;
+    static const char* _temperatureTwelfthFactName;
+
 private:
     //Fact _indexFact;
 
@@ -148,6 +197,10 @@ private:
     Fact _rpmSixthFact;
     Fact _rpmSeventhFact;
     Fact _rpmEightFact;
+    Fact _rpmNinthFact;
+    Fact _rpmTenthFact;
+    Fact _rpmEleventhFact;
+    Fact _rpmTwelfthFact;
 
     Fact _currentFirstFact;
     Fact _currentSecondFact;
@@ -157,6 +210,10 @@ private:
     Fact _currentSixthFact;
     Fact _currentSeventhFact;
     Fact _currentEightFact;
+    Fact _currentNinthFact;
+    Fact _currentTenthFact;
+    Fact _currentEleventhFact;
+    Fact _currentTwelfthFact;
 
     Fact _voltageFirstFact;
     Fact _voltageSecondFact;
@@ -166,6 +223,10 @@ private:
     Fact _voltageSixthFact;
     Fact _voltageSeventhFact;
     Fact _voltageEightFact;
+    Fact _voltageNinthFact;
+    Fact _voltageTenthFact;
+    Fact _voltageEleventhFact;
+    Fact _voltageTwelfthFact;
 
     Fact _temperatureFirstFact;
     Fact _temperatureSecondFact;
@@ -175,4 +236,8 @@ private:
     Fact _temperatureSixthFact;
     Fact _temperatureSeventhFact;
     Fact _temperatureEightFact;
+    Fact _temperatureNinthFact;
+    Fact _temperatureTenthFact;
+    Fact _temperatureEleventhFact;
+    Fact _temperatureTwelfthFact;
 };
